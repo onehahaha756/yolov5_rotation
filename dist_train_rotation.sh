2228d7d5347197d255799ff225f0e3f8539fa8ba
root <yaodongpan2019@ia.ac.cn>
2021-09-12 14:28:19 +0000

    new train param

--- dist_train_rotation.sh
+++ dist_train_rotation.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node 2 
 --weights yolov5s.pt \
 --img 640 \
 --workers 16 \
---name seaship_SkewiouLoss/yolov5s_seaship_augment_skewiou_loss-v2-640-300-0.3- \
+--name seaship_SkewiouLoss/yolov5s_seaship_augment_skewiou_loss-v2-640-300-0_15- \
 --cfg models/yolov5s_seaship.yaml \
 --data data/seaship_rotation.yaml     \
 --hyp data/hyp.scratch.yaml \
