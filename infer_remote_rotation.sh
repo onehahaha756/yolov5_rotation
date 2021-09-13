17dd0d25506f6c6527324258c16ffaba31541c0e
root <yaodongpan2019@ia.ac.cn>
2021-09-13 11:16:14 +0000

    update

--- infer_remote_rotation.sh
+++ infer_remote_rotation.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python3 detect_big_rotation.py \
---weights runs/train/SeashipsV2/yolov5s_seaship_augment_l1_loss-v2-640-300-ratio0_15-/weights/best.pt  \
+--weights runs/train/seaships/seaship38/weights/best.pt  \
 --dataset data/seaship_rotation.yaml \
 --project runs/detect/rotation_ship \
 --conf 0.01 \
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=1 python3 detect_big_rotation.py \
 --imgsz 4096 \
 --overlap 0 \
 --remote \
-
+--nosave 
 #--eval runs/dotav2/exp69/results.pkl \
 #--source /data/03_Datasets/CasiaDatasets/Ship/MixShipV3/test_seaship/images \
 #--annot_dir /data/03_Datasets/CasiaDatasets/Ship/MixShipV3/test_seaship/labelTxt 
